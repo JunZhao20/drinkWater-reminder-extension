b0d839233c77d9b93b7572110ddac3ee6cc4dfb3
Jun <j.zhao1@newcastle.ac.uk>
2023-11-09 10:46:08 +0000

    Added necessary elements as variables

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+// Elements
 const infoIcon = document.querySelector(".info");
 const hide = document.querySelector(".hide");
 const infoOverlay = document.querySelector(".info-overlay");
 const infoBox = document.querySelector(".info-box");
+const drinkNum = document.querySelector(".drink-num");
+const resetBtn = document.querySelector(".resetBtn");
+const btn = document.querySelector(".btn");
+const inputField = document.querySelector("#field-time");
 
+// Event Listener
 infoIcon.addEventListener("mouseover", () => {
   hide.style.opacity = "1";
   infoOverlay.style.transition = "opacity 0.3s ease";
