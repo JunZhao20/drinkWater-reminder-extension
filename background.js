2870c8fa8a3d62fb4501cb7abcb7b7d3ca60903b
Jun <j.zhao1@newcastle.ac.uk>
2023-11-16 10:15:28 +0000

    Correct name

--- background.js
+++ background.js
@@ -22,7 +22,7 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 // initialize default values to the timer and isRunning keys if they don't exist
 chrome.storage.local.get(["timer", "isRunning"], (res) => {
   chrome.storage.local.set({
-    timer: "timerD" in res ? res.timer : 0,
+    timer: "timer" in res ? res.timer : 0,
     isRunning: "isRunning" in res ? res.isRunning : false,
   });
 });
