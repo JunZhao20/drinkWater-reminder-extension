993a534c078605da05e2ca812b73f55dddb5edf9
Jun <j.zhao1@newcastle.ac.uk>
2023-11-30 17:01:57 +0000

    Created new notification for drink goal

--- background.js
+++ background.js
@@ -13,29 +13,46 @@ const createNotification = function () {
   });
 };
 
+const drinkFinish = function () {
+  chrome.notifications.create({
+    type: "basic",
+    title: "You are fully HYDRATED!",
+    message: "You have reached your goal.",
+    iconUrl: "./images/bottle.png",
+  });
+};
+
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "drinkTimer") {
     // creates the res obj with timer and isRunning keys.
-    chrome.storage.local.get(["timer", "isRunning", "minute"], (res) => {
-      // if isRunning is present is true initialize timer var with res.timer + 1
-      if (res.isRunning) {
-        let timer = Number(res.timer) + 1;
-        let minute = Number(res.minute);
-        if (timer === minute * 60) {
-          console.log("stopped");
-          createNotification();
+    chrome.storage.local.get(
+      // Added drinkNum to allow background service to track user's drink num
+      ["timer", "isRunning", "minute", "drinkNum"],
+      (res) => {
+        // if isRunning is present is true initialize timer var with res.timer + 1
+        if (res.isRunning) {
+          let timer = Number(res.timer) + 1;
+          let minute = Number(res.minute);
+          let drinkNum = 0;
+          if (timer === minute * 60) {
+            console.log("stopped");
+            createNotification();
+            chrome.storage.local.set({
+              isRunning: false,
+              timer: 0,
+              drinkNum: drinkNum++,
+            });
+            if (drinkNum === 7) {
+            }
+          }
+          // updates timer by setting timer var.
           chrome.storage.local.set({
-            isRunning: false,
-            timer: 0,
+            timer,
+            minute,
           });
         }
-        // updates timer by setting timer var.
-        chrome.storage.local.set({
-          timer,
-          minute,
-        });
       }
-    });
+    );
   }
 });
 // initialize default values to the timer and isRunning keys if they don't exist
@@ -44,5 +61,6 @@ chrome.storage.local.get(["timer", "isRunning"], (res) => {
     timer: "timer" in res ? Number(res.timer) : 0,
     isRunning: "isRunning" in res ? res.isRunning : false,
     minute: "minute" in res ? Number(res.minute) : 0,
+    drinkNum: "drinkNum" in res ? Number(res.drinkNum) : 0,
   });
 });
